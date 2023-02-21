f4e2cf4973594497a54cd735d2a8cdc6272dc1b1
Washington Acero M <shin321654987@gmail.com>
2023-02-21 12:00:10 -0800

    aded custom boostrap builder

--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -11,7 +11,7 @@ const stylesPath = '../src/scss/styles.scss';
 const destPath = upath.resolve(upath.dirname(__filename), '../dist/css/styles.css');
 
 module.exports = function renderSCSS() {
-    
+
     const results = sass.renderSync({
         data: entryPoint,
         includePaths: [
@@ -40,3 +40,34 @@ const entryPoint = `/*!
 */
 @import "${stylesPath}"
 `
+
+//
+
+function renderSCSSFile(sourceFile,destPathFile) {
+
+    const results = sass.renderSync({
+        file: sourceFile,
+        includePaths: [
+            upath.resolve(upath.dirname(__filename), '../node_modules')
+        ],
+    });
+
+    const destPathDirname = upath.dirname(destPathFile);
+    if (!sh.test('-e', destPathDirname)) {
+        sh.mkdir('-p', destPathDirname);
+    }
+
+    postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
+        result.warnings().forEach(warn => {
+            console.warn(warn.toString())
+        })
+        fs.writeFileSync(destPathFile, result.css.toString());
+    })
+
+};
+
+
+const bootstrapSrcPath = upath.resolve(upath.dirname(__filename), '../src/scss/bootstrap.scss');
+const bootstrapDestPath = upath.resolve(upath.dirname(__filename), '../dist/css/bootstrap.css');
+
+renderSCSSFile(bootstrapSrcPath, bootstrapDestPath);
